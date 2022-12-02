fa3ea55e8bbed5eedc8b637a30be29b76ae6b1c3
jayesh <jayeshrajpatil143@gmail.com>
2022-12-02 09:49:02 +0000

    Create hello.js and package.json

---
+++ hello.js
@@ -0,0 +1,50 @@
+console.log("Hello World");
+console.log(typeof 23455);
+console.log(typeof 2386896583784798);
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
+
+// if else
+const a = 110;
+const b= 12;
+if(a > b)
+{
+    console.log("no. is greater");
+}
+else
+{
+    console.log("false condition.");
+}
+
+// for loop
+const n=10;
+for(let i= 0; i< n ; i++)
+{
+    console.log(i);
+}
+// errot handling
+try
+{
+    // create error
+    throw new Error("Jayesh is boring");
+}
+
+catch(err)
+{
+    console.log(err);
+} 
+
+// create fun.
+
+function square(x)
+{
+
+  return x * x
+}
+  console.log(square(22)
+    )
+
+    // npmi cowsay
+   
