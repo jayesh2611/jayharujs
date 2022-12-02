0a2b50d205d12db1bbe0a2fcb0204c7fb010e221
jayesh <jayeshrajpatil143@gmail.com>
2022-12-02 10:07:23 +0000

    Update 4 files

---
+++ proj.js
@@ -0,0 +1,17 @@
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
+
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "Jayesh is coding";
+    e : "oO",
+    T : "U "
+}));
+
